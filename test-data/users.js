65faa0162a142e43a0408770512a664f0d01c534
Danil <danil.vereda247@gmail.com>
2026-02-04 13:24:53 +0200

    Add tests for sessions, update UserController

--- test-data/users.js
+++ test-data/users.js
@@ -4,6 +4,10 @@ const users = {
     password: 'Qwerty_123!',
     userId: '0fb836c0-0604-473f-bac0-c3e32040fc74',
   },
+  user2: {
+    username: `testUser_${Date.now()}`,
+    password: 'Qwerty_123!',
+  },
 };
 
 module.exports = { users };
